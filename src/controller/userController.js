c303266c167e0d5955c6b3d721042a3afc4dbcdf
sport1028 <sport1028kr@naver.com>
2021-08-02 11:28:59 +0900

    login commit

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Create Account" });
 export const postJoin = async (req, res) => {
@@ -47,15 +48,25 @@ export const postJoin = async (req, res) => {
 export const getLogin = (req,res) => res.render("login", {pageTitle: "Login"});
 export const postLogin = async (req, res) => {
     const {username, password} = req.body;
-    const exists = await User.exists({ username });
-    if(!exists) {
+    const pageTitle = "Login";
+    const user = await User.findOne({ username });
+    if(!user) {
         return res.status(400).render("login", {
-            pageTitle: "Login", 
+            pageTitle,
             errorMessage: "An account with this username does not exists.",
         });
     }
-
-    res.end();
+    const ok = await bcrypt.compare(password, user.password);
+    if (!ok){
+        return res.status(400).render("login", {
+            pageTitle, 
+            errorMessage: "Wrong password",
+        });
+    }
+    console.log("LOG USER IN! COMMING SOON!!");
+    req.session.loggedIn = true;
+    req.session.user = user;
+    return res.redirect("/");
 }
 
 export const edit = (req, res) => res.send("Edit User");
--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 import express from "express";
-
 import logger from "morgan";
+import session from "express-session";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
+import { localsMiddleware } from "./middlewares";
 
 const app = express();
 
@@ -15,6 +16,15 @@ app.set("views", process.cwd() + "/src/views");
 
 app.use(logger("dev"));
 
+app.use(
+    session({
+        secret:"Hello!",
+        resave:true,
+        saveUninitialized: true,
+    })
+);
+
+app.use(localsMiddleware);
 app.use("/", rootRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
